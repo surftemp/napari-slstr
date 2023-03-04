fa3765e5b757ef783ec06e6c492ab93691f88f7e
Niall McCarroll <niall_mccarroll@yahoo.com>
2023-03-04 19:08:06 +0000

    support #1 and #2, also display imagery the right way up

--- README.md
+++ README.md
@@ -37,7 +37,7 @@ pip install -e .
 Check that the tool will open, requires an unzipped Sentinel3 SLSTR scene, eg
 
 ```
-image_labeller ~/Projects/napari/S3A_SL_1_RBT____20170122T094902_20170122T095202_20181004T064548_0179_013_264______LR1_R_NT_003.SEN3
+image_labeller ~/Projects/napari/S3A_SL_1_RBT____20170122T094902_20170122T095202_20181004T064548_0179_013_264______LR1_R_NT_003.SEN3 ./resources/example.cfg
 ```
 
 ![napari](https://user-images.githubusercontent.com/58978249/220682442-4c52e903-8409-4888-a36c-d14fd1062e9d.png)
@@ -59,52 +59,8 @@ git pull
 By Default all channels are shown, with nadir and oblique views, plus a false colour view constructed from bands S3 (red), S2(green) and S1(blue).
 
 ```
-image_labeller <path-to-SLSTR-scene-folder> 
+image_labeller <path-to-SLSTR-scene-folder> <path-to-napari-slstr-cfg>
 ```
 
-### Specifying label layers
 
-Label layers allow a scene to be annotated on a pixel-by-pixel basis.  
-
-To enable this, create a configuration file - see [resources/example_label_layers.cfg](resources/example_label_layers.cfg) for an example - and pass its path using the `--label-layers-path` option:
-
-```
-image_labeller --label-layers-path=resources/example_label_layers.cfg
-```
-
-### Selecting bands
-
-To display only bands S4,S5 and S6:
-
-```
-image_labeller --bands "S4,S5,S6"
-```
-
-To display only bands S4, S5 and S6 plus a FalseColour band based on these bands:
-
-```
-image_labeller --bands "S4,S5,S6,FalseColour:S4:S5:S6"
-```
-
-Note that the order in which bands are listed determines the order of the layers in Napari.  Later bands are mapped to higher layers.
-
-### Turning off the oblique view
-
-By default, each band creates two layers, nadir and oblique.  The oblique view is narrower.  To disable the oblique views specify the `--no-oblique` option
-
-```
-image_labeller --no-oblique
-```
-
-### Configuring the default colour maps
-
-By default, the viridis colour map will be used for radiance bands and the magma colour map will be used for BT bands
-
-To change this, use the `--radiance-colourmap` and `--bt-colourmap` options:
-
-```
-image_labeller --bt-colourmap=hsv --radiance-colourmap=turbo
-```
-
-...where the colourmap names can be listed in the napari UI 
 
--- src/napari_slstr/image_labeller.py
+++ src/napari_slstr/image_labeller.py
@@ -1,8 +1,30 @@
+# MIT License
+#
+# Copyright (c) 2023 surftemp
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import napari
 import xarray as xr
 import numpy as np
 import os.path
-import sys
+import logging
 
 try:
     import tomllib
@@ -10,146 +32,218 @@ except ModuleNotFoundError:
     # python < 3.11
     import tomli as tomllib
 
-DEFAULT_BANDS="S1,S2,S3,S4,S5,S6,S7,S8,S9,FalseColour:S3:S2:S1"
+# work out offsets for superimposing the narrow oblique grid on the wider nadir grid
+# TODO shout this be calculated from each scene's manifest file?
+OBLIQUE_OFFSET_RADIANCE = 1096  # 0.5km/trackOffset/nadir=1996 - 0.5km/trackOffset/oblique=900
+OBLIQUE_OFFSET_BT = 548  # 1km/trackOffset/nadir=998 - 1km/trackOffset/oblique=450
 
-OBLIQUE_OFFSET_RADIANCE = 1096 # 0.5km/trackOffset/nadir=1996 - 0.5km/trackOffset/oblique=900
-OBLIQUE_OFFSET_BT = 548        # 1km/trackOffset/nadir=998 - 1km/trackOffset/oblique=450
 
 class SceneLabeler:
 
-    def __init__(self, scene_path, label_layers=None):
+    def __init__(self, scene_path, layers):
+        """
+        Create a SceneLabeller object to manage the loading of data into and saving of label data out of napari,
+        given an SLSTR scene L1C
+        :param scene_path: path to the scene
+        :param layers: a dictionary controlling which layers to load, with layer names as keys and layer settings as values.
+
+        Loaded from the configuration file.  See comments in resources/example.cfg
+        """
         self.scene_path = scene_path
         self.viewer = napari.Viewer()
-        self.data_dims = ("rows","columns")
-        self.data_shape = (1200,1500)
+        # record the expected dimensions and shape for the 1km nadir grid
+        self.data_dims = ("rows", "columns")
+        self.data_shape = (1200, 1500)
+        self.masks = {}  # populated on open
+        self.layers = layers
 
-        self.masks = {} # populated on open
-        self.label_layers = label_layers
+    def coarsen(self, da, aggregate):
+        """
+        Convert an array from the 0.5km to the 1km grid, using the specified aggregation method on each group of 4 pixels to be merged
+        :param da: an xarray.DataArray object containing 0.5km grid
+        :param aggregate: the aggregate function to apply, either "mean" or "sdev"
+        :return: an xarray.DataArray object containing aggregated data on the 1km grid
+        """
 
-    def coarsen(self,da):
-        return da.coarsen(rows=2,columns=2).mean()
+        if aggregate == "mean":
+            return da.coarsen(rows=2, columns=2).mean()
+        elif aggregate == "sdev":
+            return da.coarsen(rows=2, columns=2).std()
+        else:
+            raise ValueError(f'Invalid aggregate name {aggregate} should be either "mean" or "sdev"')
 
-    def add_image_layer(self,channel,name='',cmap="viridis"):
-        coarsen = False
-        pad = False
-        if "radiance" in channel:
-            coarsen = True
-        if channel.endswith("o"):
-            pad = True
+    def add_image_layer(self, channel, variable, name, cmap="viridis", aggregate="mean"):
+        """
+        Add a single channel as an image layer
+        :param channel: filename, without the .nc suffix eg. S3_radiance_an, S8_BT_in etc
+        :param variable: the name of the variable inside the channel file
+        :param name: the name to give the layer in napari
+        :param cmap: the name of the napart colour map to use, eg magma, viridis
+        :param aggregate: an aggregate function to use if the data needs to be aggregated to the 1km grid
+        """
+        channel_path = os.path.join(self.scene_path, channel + ".nc")
+        if not os.path.exists(channel_path):
+            raise FileNotFoundError(channel_path)
 
-        if not name:
-            name = channel
-        ds = xr.open_dataset(os.path.join(self.scene_path,channel + ".nc"))
-        da = ds[channel].squeeze()
-        if pad:
-            if coarsen:
+        # channel should be named
+        # SN_(radiance|BT)_(a|b|i)(n|o)
+        oblique = channel.endswith("o")
+
+        ds = xr.open_dataset(channel_path)
+        da = ds[variable].squeeze()
+        if oblique:
+            if da.data.shape == (2400, 1800):
+                # 500m grid
                 data = np.zeros(shape=(2400, 3000))
                 data[:, :] = np.nan
-                data[:,OBLIQUE_OFFSET_RADIANCE:OBLIQUE_OFFSET_RADIANCE+1800] = da.data
-                da = xr.DataArray(data,dims=("rows","columns"))
+                data[:, OBLIQUE_OFFSET_RADIANCE:OBLIQUE_OFFSET_RADIANCE + 1800] = da.data
+                da = xr.DataArray(data, dims=("rows", "columns"))
             else:
-                data = np.zeros(shape=(1200,1500))
+                # 1km grid
+                data = np.zeros(shape=(1200, 1500))
                 data[:, :] = np.nan
                 data[:, OBLIQUE_OFFSET_BT:OBLIQUE_OFFSET_BT + 900] = da.data
                 da = xr.DataArray(data, dims=("rows", "columns"))
 
-        if coarsen:
-            da = self.coarsen(da)
+        if da.data.shape == (2400, 3000):
+            da = self.coarsen(da, aggregate)
 
         if da.data.shape != self.data_shape:
-            raise TypeError(f"Data for channel {channel} has incompatible shape: {da.data.shape} should be {self.data_shape}")
+            raise TypeError(
+                f"Data for channel {channel} has incompatible shape: {da.data.shape} should be {self.data_shape}")
 
-        self.viewer.add_image(da.data, name=name, colormap=cmap)
+        self.viewer.add_image(np.flip(da.data,axis=0), name=name, colormap=cmap)
 
-    def add_rgb_layer(self,name,red_channel,green_channel,blue_channel,coarsen=True):
+    def add_rgb_layer(self, name, rgb_channels, rgb_variables, aggregate="mean"):
+        """
+        Add a set of 3 channels to form a FalseColour RGB layer
+        :param name: the name of the layer to display in napari
+        :param rgb_channels: a list of three channel names, [red-channel,green-channel,blue-channel] each channel is the filename without .nc suffix
+        :param rgb_variables: a list of three variable names, [red-channel,green-channel,blue-channel] to load from each channel
+        :param aggregate: an aggregate function to use if the data needs to be aggregated to the 1km grid
+        """
         rgb_arrays = []
-        for channel in [red_channel,green_channel,blue_channel]:
+        for idx in range(3):
+            channel = rgb_channels[idx]
+            channel_path = os.path.join(self.scene_path, channel + ".nc")
+            if not os.path.exists(channel_path):
+                raise FileNotFoundError(channel_path)
+
+            variable = rgb_variables[idx]
             ds = xr.open_dataset(os.path.join(self.scene_path, channel + ".nc"))
-            da = ds[channel].squeeze()
-            if coarsen:
-                da = self.coarsen(da)
+            da = ds[variable].squeeze()
+            if da.data.shape == (2400, 3000):
+                da = self.coarsen(da, aggregate)
             max_radiance = np.nanmax(da.data)
             da = da / max_radiance
+
+            if da.data.shape != self.data_shape:
+                raise TypeError(
+                    f"Data for channel {channel} has incompatible shape: {da.data.shape} should be {self.data_shape}")
+
             rgb_arrays.append(da)
 
-        rgb_data = xr.concat(rgb_arrays,"rgb")
-        rgb_data = rgb_data.transpose(*rgb_data.dims[1:]+rgb_data.dims[0:1])
-        self.viewer.add_image(rgb_data.data,name=name,rgb=True)
+        rgb_data = xr.concat(rgb_arrays, "rgb")
+        rgb_data = rgb_data.transpose(*rgb_data.dims[1:] + rgb_data.dims[0:1])
+        self.viewer.add_image(np.flip(rgb_data.data,axis=0), name=name, rgb=True)
 
-    def open(self, bands, use_oblique_view=True, radiance_colourmap="", bt_colourmap=""):
-        for band in bands:
-            if band in ["S7","S8","S9"]:
-                if radiance_colourmap:
-                    self.add_image_layer(f"{band}_BT_in", cmap=bt_colourmap)
-                    if use_oblique_view:
-                        self.add_image_layer(f"{band}_BT_io", cmap=bt_colourmap)
-            elif band in ["S1", "S2", "S3", "S4", "S5", "S6"]:
-                if bt_colourmap:
-                    self.add_image_layer(f"{band}_radiance_an", cmap=radiance_colourmap)
-                    if use_oblique_view:
-                        self.add_image_layer(f"{band}_radiance_ao", cmap=radiance_colourmap)
-            elif band.startswith("FalseColour"):
-                fc_bands = list(map(lambda b:b.strip(),band.split(":")))[1:]
-                if len(fc_bands) != 3:
-                    print("Please specify three FalseColour bands, for example FalseColour:S3:S2:S1")
-                    sys.exit(-1)
-                for fc_band in fc_bands:
-                    if fc_band not in ["S1","S2","S3","S4","S5","S6"]:
-                        print(f"Invalid FalseColour band {fc_band}, should be one of S1,S2,S3,S4,S5,S6")
-                self.add_rgb_layer("False Colour S3/S2/S1 RGB", *list(map(lambda b: b+"_radiance_an",fc_bands)))
+    def open(self):
+        """
+        Open the SLSTR scene files and display in napari
+        """
+        for (layer_name, layer) in self.layers.items():
+
+            layer_type = layer["type"]
+            enabled = layer.get("enabled", True)
+            if enabled:
+                print(f"Adding layer {layer_name}:{layer_type} ... ", end="")
             else:
-                print(f"Ignoring unknown band {band}.")
-                sys.exit(-1)
+                continue
 
-        if self.label_layers:
-            for name in self.label_layers:
-                label_layer = self.label_layers[name]
-                filename = label_layer["filename"]
-                colour = label_layer["colour"]
-                path = os.path.join(self.scene_path,filename)
-                if os.path.exists(path):
-                    self.masks[name] = xr.open_dataarray(path)
+            try:
+                if layer_type == "image":
+                    colourmap = layer["colourmap"]
+                    filename = layer["filename"]
+                    filename_root = os.path.splitext(filename)[0]
+                    aggregate = layer.get("aggregate", "mean")
+                    variable = layer.get("variable", filename_root)
+                    self.add_image_layer(filename_root, variable, layer_name, cmap=colourmap, aggregate=aggregate)
+
+                elif layer_type == "rgb_image":
+                    red_filename = layer["red_filename"]
+                    green_filename = layer["green_filename"]
+                    blue_filename = layer["blue_filename"]
+                    root_filenames = [os.path.splitext(filename)[0] for filename in
+                                      [red_filename, green_filename, blue_filename]]
+                    red_variable = layer.get("red_variable", root_filenames[0])
+                    green_variable = layer.get("green_variable", root_filenames[1])
+                    blue_variable = layer.get("blue_variable", root_filenames[2])
+                    aggregate = layer.get("aggregate", "mean")
+                    self.add_rgb_layer(layer_name, rgb_channels=root_filenames,
+                                       rgb_variables=[red_variable, green_variable, blue_variable], aggregate=aggregate)
+
+                elif layer_type == "label":
+                    colour = layer["colour"]
+                    filename = layer["filename"]
+                    path = os.path.join(self.scene_path, filename)
+                    if os.path.exists(path):
+                        self.masks[layer_name] = xr.open_dataarray(path)
+                    else:
+                        self.masks[layer_name] = xr.DataArray(data=np.zeros(dtype=int, shape=self.data_shape),
+                                dims=self.data_dims, name=layer_name, attrs={"description":"mask for label: "+layer_name})
+                    self.viewer.add_labels(np.flip(self.masks[layer_name].data,axis=0), name=layer_name + " (" + colour + ")",
+                                           num_colors=1, color={1: colour})
+
                 else:
-                    self.masks[name] = xr.DataArray(data=np.zeros(dtype=int,shape=self.data_shape),dims=self.data_dims)
-                layer_name = "Label-"+name+"("+colour+")"
-                self.viewer.add_labels(self.masks[name].data, name=layer_name, num_colors=1, color={1:colour})
+                    raise ValueError(f"Unknown layer type {layer_type}")
+                print(f"[Done]")
+            except Exception as ex:
+                print("[Failed]")
+                logging.exception(f"Failed to add layer {layer_name}:{layer_type}")
+
         napari.run()
 
     def save(self):
-        if self.label_layers:
-            for name in self.label_layers:
-                label_layer = self.label_layers[name]
-                filename = label_layer["filename"]
-                path = os.path.join(self.scene_path,filename)
-                self.masks[name].close()
-                self.masks[name].to_netcdf(path)
+        """
+        Save the labels data to files
+        """
+        for (layer_name, layer) in self.layers.items():
+            if layer["type"] == "label":
+                filename = layer["filename"]
+                path = os.path.join(self.scene_path, filename)
+                print(f"Saving labels from {layer_name} to {path}... ", end="")
+                try:
+                    self.masks[layer_name].close()
+                    self.masks[layer_name].to_netcdf(path)
+                    print("[Done]")
+                except Exception as ex:
+                    print("[Failed]")
+                    logging.exception("Failed to write labels file")
 
 
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("scene_path", help="path to an SLSTR scene to label")
-    parser.add_argument("--label-layers-path", help="Specify the path to a file configuring label layers")
-    parser.add_argument("--bands", help="specify a comma separated list of bands to visualise, defaults to all bands."
-                        "To specify a false colour band, use FalseColour:SX:SY:SZ", default=DEFAULT_BANDS)
-    parser.add_argument("--no-oblique", help="Only show nadir views, not oblique ones", action="store_true")
-    parser.add_argument("--radiance-colourmap", help="Colourmap for radiance channels.  "
-                        "Set to blank to suppress all radiance channels", default="viridis")
-    parser.add_argument("--bt-colourmap", help="Colourmap for BT channels. " 
-                        "Set to blank to suppress all BT channels", default="magma")
+    parser.add_argument("cfg_path", help="Specify the path to a file configuring label layers")
+
+    logging.basicConfig(level=logging.INFO)
 
     args = parser.parse_args()
 
-    label_layers = None
-    if args.label_layers_path:
-        with open(args.label_layers_path) as f:
-            label_layers = tomllib.loads(f.read())
+    # load the configuration defining each layer to display
+    with open(args.cfg_path) as f:
+        layers = tomllib.loads(f.read())
 
-    labeler = SceneLabeler(args.scene_path,label_layers=label_layers)
-    bands = map(lambda b:b.strip(),args.bands.split(","))
-    labeler.open(bands, use_oblique_view=not args.no_oblique,
-                 radiance_colourmap=args.radiance_colourmap, bt_colourmap=args.bt_colourmap)
-    labeler.save()
+    labeler = SceneLabeler(args.scene_path, layers)
+
+    # load and open the layers.  catch exceptions and try to save updated labels
+    try:
+        labeler.open() # exits when the user closes the napari window
+    except Exception as ex:
+        logging.exception("Error when opening napari")
+    labeler.save() # save labels back to file
+
 
 if __name__ == '__main__':
     main()
